476db7e43007db0a7b8e926c077328162debd9f7
sejdemi <sejan.miah@gmail.com>
2016-12-05 16:32:53 -0500

    Class Car created

---
+++ Practice/Practice/DataModel.swift
@@ -0,0 +1,20 @@
+//
+//  DataModel.swift
+//  Practice
+//
+//  Created by Sejan Miah on 12/5/16.
+//  Copyright © 2016 Sejan Miah. All rights reserved.
+//
+
+import Foundation
+
+
+class Car {
+    
+    var brand: String
+    
+    init(brand: String) {
+        self.brand = brand
+        
+    }
+}
